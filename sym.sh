43f99e613a5b48f363c3447fc1c090515d4205c4
jacob tipp <jacobtipp@gmail.com>
2015-07-29 22:39:27 -0700

    initial commit

---
+++ sym.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+update () {
+  files=($(ls bin/dotfiles/sym))
+  for f in "${files[@]}" 
+  do
+    if [ -f ~/."$f" -o -d ~/."$f" ]
+    then
+      # remove symlink from home directory
+      rm -rf ~/."$f" 
+    fi
+    # symlink dotfiles from git repo
+    ln -s -i bin/dotfiles/sym/"$f" ~/."$f"
+  done
+
+  # update vim submodules
+  cd bin/dotfiles/sym/vim 
+  git submodule init
+  git submodule update
+}
+
+create () {
+  # make bin directory
+  mkdir ~/bin 
+
+  # symlink bin folder with dotfiles
+  ln -s -i $(pwd)/bin/dotfiles ~/bin
+  update
+}
+
+if [ -d ~/bin ]
+then
+  echo 'Updating dotfiles'
+  update
+else
+  echo 'Creating dotfiles'
+  create
+fi
